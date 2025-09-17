530dbe91f33ae478fdb9e4dccc591d244d44e899
serg2rax <g.spam.reg.box@gmail.com>
2025-09-17 20:22:29 +1000

    Clean test print techers

--- src/main.py
+++ src/main.py
@@ -15,12 +15,32 @@ import sys
 from tabulate import tabulate
 from .args import args
 
+
 class teacher:
-   def __init__(self, name):
+    def __init__(self, name):
         self.name = name
         self.subject = []
-   def add_item(self, subject):
-       self.subject.append(subject)
+    def add_subj(self, subject):
+        self.subject.append(subject)
+    def set_name(self, name):
+        self.name = name
+    def get_name(self):
+        return self.name
+    def get_subj(self):
+        return self.subject
+
+class student:
+    def __init__(self, name):
+        self.name = name
+        self.grade = []
+    def add_grade(self, grade):
+        self.grade.append(grade)
+    def set_name(self, name):
+        self.name = name
+    def get_name(self):
+        return self.name
+    def get_grade(self):
+        return self.grade
 
 tabs = []
 
@@ -37,15 +57,30 @@ def main():
     tab = {i: i for i in tabs[0].keys()}
     print(tabulate(tabs, tab, "grid"))
 
-    for i in tabs:
-        teacher([i['teacher_name']]);
-        print(i['teacher_name'], i['subject'])
+    teachers = []
+    for i in range(len(tabs)):
+        teachers.append( teacher(tabs[i]['teacher_name']))
+        name = teachers[i].get_name()
+        if(tabs[i]['teacher_name'] == name):
+            teachers[i].add_subj(tabs[i]['subject'])
+            print(teachers[i].get_name(), teachers[i].get_subj())
+
+    students = []
+    students.append(student(tabs[0]['student_name']))
+    for i in range(len(tabs)):
+        students.append(student(tabs[i]['student_name']))
+        name = students[i].get_name()
+        for row in tabs:
+            if(row['student_name'] == name):
+                students[i].add_grade(row['grade'])
+
+    for row in students:
+        print(row.get_name(), row.get_grade())
 
     with open(args.report, 'w') as fd:
         writer = csv.writer(fd, delimiter=' ',
           quotechar=',', quoting=csv.QUOTE_MINIMAL)
         writer.writerows(tabs)
-
 
 if __name__ == "__main__" :
     main()
