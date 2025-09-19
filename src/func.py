54304474f56ff248db147999cad01e56c23c3fb2
Serg_mini <serg2ak@ya.ru>
2025-09-19 14:02:13 +1000

    move funcition to module

---
+++ src/func.py
@@ -0,0 +1,62 @@
+"""
+Generation student performance analysis report
+
+Written specifically for https://work-mate.ru
+Description task: https://docs.google.com/document/d/\
+1Wq7ALJDhin2uY5ldh4srWQFsfJxWUZK_SK4INIwUWug
+
+Author : Sergey Alexadrovich Kravchuk
+Email  : serg2ak@ya.ru
+"""
+
+from tabulate import tabulate
+from .args import args
+from .classes import *
+from .db import *
+
+htab_student_avg = ['№', 'name', 'AVG']
+htb_teacher = 'teacher_name'
+htb_student = 'student_name'
+htb_subject = 'subject'
+htb_grade = 'grade'
+
+def gen_lstudents(tabs:dict)->list:
+    students = []
+    for name in gen_ddlist(tabs, htb_student):
+        students.append(student(name))
+    return students
+
+def gen_lteachers(tabs:dict)->list:
+    teachers = []
+    for name in gen_ddlist(tabs, htb_teacher):
+        teachers.append(teacher(name))
+    return teachers
+
+def add_subject_teacher(tabs:dict)->list:
+    teachers = gen_lteachers(tabs) 
+    for i in range(len(teachers)):
+        for k in range(len(tabs)):
+            if ( (tabs[k][htb_teacher] == teachers[i].get_name())\
+                and (tabs[k][htb_subject] not in teachers[i].get_subj()) ):
+                teachers[i].add_subj(tabs[k][htb_subject])
+    return teachers
+
+def add_grade_student(tabs:dict)->list:
+    students = gen_lstudents(tabs)
+    for i in range(len(students)):
+        for k in range(len(tabs)):
+            if ( tabs[k][htb_student] == students[i].get_name() and tabs[k]['grade']):
+                students[i].add_grade({tabs[k][htb_subject]: tabs[k][htb_grade]})
+    return students
+
+def report_avg_student(students, htab_student_avg)->list:
+    stud = []
+    for i in range(len(students)):
+        stud.append([i, students[i].get_name(), str(students[i].get_avg())])
+    print(tabulate(stud, htab_student_avg, "grid"))
+    return stud
+
+def file_report_student(stud, htab_student_avg):
+    with open(args.report, 'w', encoding="utf8") as fd:
+        print(tabulate(stud, htab_student_avg, "grid"), file=fd)
+    return True
