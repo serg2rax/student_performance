6d9d14a234c57d9e4b3b728a915e0a207677a3b3
Serg_mini <serg2ak@ya.ru>
2025-09-19 18:25:42 +1000

    move conf var to file

---
+++ src/conf.py
@@ -0,0 +1,16 @@
+"""
+Generation student performance analysis report
+
+Written specialy for https://work-mate.ru
+Description task: https://docs.google.com/document/d/\
+1Wq7ALJDhin2uY5ldh4srWQFsfJxWUZK_SK4INIwUWug
+
+Author : Sergey Alexadrovich Kravchuk
+Email  : serg2ak@ya.ru
+"""
+
+htab_student_avg = ['№', 'name', 'AVG']
+htb_teacher = 'teacher_name'
+htb_student = 'student_name'
+htb_subject = 'subject'
+htb_grade = 'grade'
